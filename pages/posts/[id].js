ceccbf41ff1fdd952c3c56fd027c58f75857f996
victorsmith <victor7smith@gmail.com>
2022-12-16 14:22:07 -0600

    remark

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,36 +2,33 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
-  <Layout>  
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-  </Layout>
+  console.log("Post data", postData);
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
 }
 
-export default async function getStaticPaths() {
+export async function getStaticPaths() {
   const paths = getAllPostIds();
 
   return {
     paths,
     fallback: false,
-  }
+  };
 }
 
-export default function getStaticPaths({ params }) {
-  const postData = getPostData(params.id);
-  
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-
 }
-
-
-
-
-
